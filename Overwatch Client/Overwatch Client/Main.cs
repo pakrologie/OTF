1e101f74b5b8f38ea05e27ebbc3d48436dd97a28
pakrologie <nostalelscience@gmail.com>
2016-07-14 19:29:50 +0200

    Ajout d'un message " Boutton 2 "

--- Overwatch Client/Overwatch Client/Main.cs
+++ Overwatch Client/Overwatch Client/Main.cs
@@ -21,5 +21,10 @@ namespace Overwatch_Client
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Je t'aime bb !");
+        }
     }
 }
