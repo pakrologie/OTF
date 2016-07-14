46b29d46f688e5770fe15aa614dd56b9c2e5ab82
Sl4yZ <rxzeditor@gmail.com>
2016-07-14 19:23:17 +0200

    reglage

--- Overwatch Client/Overwatch Client/Main.cs
+++ Overwatch Client/Overwatch Client/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Overwatch_Client
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
