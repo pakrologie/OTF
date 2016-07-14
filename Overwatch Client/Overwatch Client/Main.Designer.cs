0d2585623da50caebcdd6fb37438fb85b56eb33d
Sl4yZ <rxzeditor@gmail.com>
2016-07-14 19:27:57 +0200

    add button

--- Overwatch Client/Overwatch Client/Main.Designer.cs
+++ Overwatch Client/Overwatch Client/Main.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -40,9 +41,19 @@
             this.button1.Text = "Bouton 1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 160);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(581, 373);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Main";
             this.Load += new System.EventHandler(this.Main_Load);
@@ -53,6 +64,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
