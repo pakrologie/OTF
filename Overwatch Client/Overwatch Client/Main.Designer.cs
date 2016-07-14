95d05f6c3be769048f57e31edaf777f6eac8c495
pakrologie <nostalelscience@gmail.com>
2016-07-14 19:27:09 +0200

    Ajout d'un bouton

--- Overwatch Client/Overwatch Client/Main.Designer.cs
+++ Overwatch Client/Overwatch Client/Main.Designer.cs
@@ -28,11 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bouton 1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Name = "Main";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
@@ -40,6 +51,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
